acaf6b530aa1bfadb540caf3e0ef4b4cb3510ac4
rajesh maddula <rajesh.maddula21@gmail.com>
2026-07-02 11:07:31 +0530

    actions

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "igw_tags" {
 }
 variable "public_subnet_cidrs" {
     type = list
-    default = ["10.0.1.0/24","10.0.2/24"]
+    default = ["10.0.1.0/24","10.0.2.0/24"]
 }
 variable "public_subnet_tags" {
     type = map
